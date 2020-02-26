90b178b4518ba9f885de0914c50cb363a1a86b53
ehenrion <edouard.henrion@mcgill.ca>
2020-02-26 16:49:28 +0000

    GenPipes RNA-SEq - calling DESeq2 instead of DESeq for differential_expression

--- bfx/differential_expression.py
+++ bfx/differential_expression.py
@@ -40,7 +40,7 @@ def deseq(
             ['differential_expression_deseq', 'module_R']
         ],
         command="""\
-Rscript $R_TOOLS/deseq.R \\
+Rscript $R_TOOLS/deseq2.R \\
   -d {design_file} \\
   -c {count_matrix} \\
   -o {output_dir} \\
